8f9e2da40da829ddb809dfb493dc0bac4014d6f1
Matthew Fanning <matt.fanning@gmail.com>
2022-03-02 14:47:03 -0400

    Adding rules for git,gus + minor cleanup

--- src/sorting.js
+++ src/sorting.js
@@ -6,8 +6,10 @@ importScripts('/src/wrappers.js');
 class Sorting {
   static getRules() {
     return [
-      {address: "https?://www.vai.com/*", groupProperties: {title: "Guitar", color: "grey"}},
-      {address: "https?://developer.chrome.com/*", groupProperties: {title: "Chrome API"}}
+      {address: "https?://www.vai.com", groupProperties: {title: "Guitar", color: "grey"}},
+      {address: "https?://developer.chrome.com", groupProperties: {title: "Chrome API"}},
+      {address: "https?://(git.soma.salesforce.com|github.com)", groupProperties: {title: "Git", color: "green"}},
+      {address: "https?://gus.lightning.force.com", groupProperties: {title: "Gus", color: "blue"}}
     ]
   }
   static async executeOn(tab) {
@@ -18,35 +20,35 @@ class Sorting {
     for (const rule of sortRules) {
       const regex = new RegExp(rule.address)
       if(regex.test(url)) {
-      // current tab's url matches the sorting rule. Find or create group rule.groupName
-      const groups = await Wrappers.chromeTabsGroupsQuery({title: rule.groupProperties.title}) 
-      
-      if(groups.length > 0) {
-        // group already exists.  Move currentTab to it.  Re-activate currentTab
-        console.log("Group found")
-        const foundGroupId = groups[0].id
+        // current tab's url matches the sorting rule. Find or create group rule.groupName
+        const groups = await Wrappers.chromeTabsGroupsQuery({title: rule.groupProperties.title}) 
         
-        // if tab is already sorted, don't do the work
-        if(groupId !== foundGroupId) {
-          await Wrappers.chromeTabsGroup({groupId: foundGroupId, tabIds: id})
-          await Wrappers.chromeTabsUpdate(id, {active: true})
+        if(groups.length > 0) {
+          // group already exists.  Move currentTab to it.  Re-activate currentTab
+          console.log("Group found")
+          const foundGroupId = groups[0].id
+          
+          // if tab is already sorted, don't do the work
+          if(groupId !== foundGroupId) {
+            await Wrappers.chromeTabsGroup({groupId: foundGroupId, tabIds: id})
+            await Wrappers.chromeTabsUpdate(id, {active: true})
 
-          /* NOTE the current window will remain active if not empty after move!!
-            If you choose to change this later, 
-            https://developer.chrome.com/docs/extensions/reference/windows/#method-update */
+            /* NOTE the current window will remain active if not empty after move!!
+              If you choose to change this later, 
+              https://developer.chrome.com/docs/extensions/reference/windows/#method-update */
+          }
+        } 
+        else {
+          //group doesn't exist yet.  Make it.  Move currentTab to it.
+          console.log("Group not found")
+          const newGroupId = await Wrappers.chromeTabsGroup({
+            tabIds: id, 
+            createProperties: {windowId: windowId}
+          })
+          const updateProperties = rule.groupProperties
+          await Wrappers.chromeTabsGroupsUpdate(newGroupId, updateProperties)
         }
       } 
-      else {
-        //group doesn't exist yet.  Make it.  Move currentTab to it.
-        console.log("Group not found")
-        const newGroupId = await Wrappers.chromeTabsGroup({
-          tabIds: id, 
-          createProperties: {windowId: windowId}
-        })
-        const updateProperties = rule.groupProperties
-        await Wrappers.chromeTabsGroupsUpdate(newGroupId, updateProperties)
-      }
-      }
     }
   }
 }
